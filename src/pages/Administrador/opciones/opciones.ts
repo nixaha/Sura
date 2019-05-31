d7f7d76c453afbe40adb1cd59994ab6802de836e
dev-jorge <jorge.reyes@durango.gob.mx>
2019-05-31 13:39:36 -0500

    Implementación de logout

--- src/pages/Administrador/opciones/opciones.ts
+++ src/pages/Administrador/opciones/opciones.ts
@@ -1,14 +1,21 @@
 import { Component } from "@angular/core";
-import { NavController, NavParams } from "ionic-angular";
+import { NavController, NavParams, App } from "ionic-angular";
 
-import { EventosAdminPage } from "../eventos/eventos";
+import { EventosAdminPage, LogInPage } from "../../index.paginas";
+
+import { MessagesService } from "../../../services/index.services";
 
 @Component({
   selector: "page-opciones",
   templateUrl: "opciones.html"
 })
 export class OpcionesPage {
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private messagesService: MessagesService,
+    private app: App
+  ) {}
 
   ionViewDidLoad() {
     console.log("ionViewDidLoad OpcionesPage");
@@ -16,5 +23,24 @@ export class OpcionesPage {
 
   eventos() {
     this.navCtrl.push(EventosAdminPage);
+  }
+
+  logout() {
+    this.messagesService.showMessage(
+      "Cerrar sesión",
+      "¿Desea salir de su cuenta?",
+      [
+        {
+          text: "Aceptar",
+          handler: () => {
+            localStorage.clear();
+            this.app.getRootNav().setRoot(LogInPage);
+          }
+        },
+        {
+          text: "Cancelar"
+        }
+      ]
+    );
   }
 }
--- src/pages/Login/log-in/log-in.ts
+++ src/pages/Login/log-in/log-in.ts
@@ -1,46 +1,57 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-import { RegistroPage, RestablecerContraPage, TabsPage, OpcionesPage } from "../../index.paginas"
+import { Component } from "@angular/core";
+import { NavController, NavParams } from "ionic-angular";
+import {
+  RegistroPage,
+  RestablecerContraPage,
+  TabsPage,
+  OpcionesPage,
+  HomePage
+} from "../../index.paginas";
 import { User } from "../../../shared/models/user";
 
-import { LoginService, MessagesService } from '../../../services/index.services';
+import {
+  LoginService,
+  MessagesService
+} from "../../../services/index.services";
 
 @Component({
-  selector: 'page-log-in',
-  templateUrl: 'log-in.html',
+  selector: "page-log-in",
+  templateUrl: "log-in.html"
 })
 export class LogInPage {
-
   registro: any = RegistroPage;
   olvidar: any = RestablecerContraPage;
 
   user = {} as User;
-  email: string = '';
+  email: string = "";
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     private loginService: LoginService,
     private messagesService: MessagesService
-  ) {
-  }
+  ) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad LogInPage');
+    console.log("ionViewDidLoad LogInPage");
   }
 
   login() {
-    this.messagesService.showLoadingMessage('Autenticando...');
+    this.messagesService.showLoadingMessage("Autenticando...");
     this.loginService.login(this.user).then(
       result => {
-        this.loginService.getUserInfo().on('value', (snapshot) => {
+        this.loginService.getUserInfo().on("value", snapshot => {
           this.saveData(result, snapshot);
-          this.checkRole(snapshot.child('rol').val());
+          this.checkRole(snapshot.child("rol").val());
         });
         this.messagesService.hideLoadingMessage();
-      }, error => {
-        console.log(JSON.stringify(error));
-        this.messagesService.showMessage('Error', this.loginService.getLoginErrorMessage(error.code), ['Aceptar']);
+      },
+      error => {
+        this.messagesService.showMessage(
+          "Error",
+          this.loginService.getLoginErrorMessage(error.code),
+          ["Aceptar"]
+        );
         this.messagesService.hideLoadingMessage();
       }
     );
@@ -49,19 +60,18 @@ export class LogInPage {
   saveData(result, snapshot) {
     const data = {
       token: result.user.refreshToken,
-      nombre: snapshot.child('nombre').val(),
-      estado: snapshot.child('estado').val(),
-      rol: snapshot.child('rol').val()
+      nombre: snapshot.child("nombre").val(),
+      estado: snapshot.child("estado").val(),
+      rol: snapshot.child("rol").val()
     };
-    localStorage.setItem('data', JSON.stringify(data));
+    localStorage.setItem("data", JSON.stringify(data));
   }
 
   checkRole(rol) {
-    if (rol === 'PARTICIPANTE') {
+    if (rol === "PARTICIPANTE") {
       this.navCtrl.setRoot(TabsPage);
-    } else if (rol === 'ADMIN') {
+    } else if (rol === "ADMIN") {
       this.navCtrl.setRoot(OpcionesPage);
     }
   }
-
 }
